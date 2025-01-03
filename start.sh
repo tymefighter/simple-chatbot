59c66bfbf429cf336c89641f53a2c3b298c839c5
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2025-01-03 17:49:37 +0530

    [GH-4] fix: Fixes issues

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 # Build Images
 docker build apps/chat-engine -t chat-engine
+docker build apps/chat-web -t chat-web
 
 # Kafka
 kubectl create -f kubernetes/services/kafka-service.yaml
@@ -7,3 +8,11 @@ kubectl create -f kubernetes/deployments/kafka.yaml
 
 # Chat Engine
 kubectl create -f kubernetes/deployments/chat-engine.yaml
+
+# MySQL
+kubectl create -f kubernetes/services/mysql-service.yaml
+kubectl create -f kubernetes/deployments/mysql.yaml
+
+# Chat Web
+kubectl create -f kubernetes/services/chat-web-service.yaml
+kubectl create -f kubernetes/deployments/chat-web.yaml
